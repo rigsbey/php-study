c6566fbcc4ea11bdf37bf528058df9529e3796fb
Kamil <ab.kam@yandex.ru>
2018-02-08 10:40:19 +0400

    Changes for 2 homework

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+$n = 0;
+function Fibonachi ($n){
+    if($n == 1) return 1;
+    if ($n == 2) return 1;
+    if($n >= 3) return  Fibonachi($n -1) + Fibonachi($n-2);
+}
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -8,25 +16,18 @@
 <body>
 <form action="index.php" method="get">
     <b>Введите число N: </b>
-    <input type="text" name="press">
-    <input  type="submit" value="send">
+    <input  type="text" name="press">
+    <input  type="submit" value="send"><br><br>
+    <?$a = ($_GET['press']);
+
+    for($i = 0; $i <= $a; $i++){
+    echo Fibonachi($i);
+    echo "  ";
+    }
+    ?>
 </form>
 
-<?php
-$a = ($_GET['press']);
-$n = 0;
- function Fibonachi ($n){
-    if($n == 1) return 1;
-    if ($n == 2) return 1;
-    if($n >= 3) return  Fibonachi($n -1) + Fibonachi($n-2);
-}
 
-for($i = 0; $i <= $a; $i++){
-    echo Fibonachi($i);
-    echo " ";
-}
-
-?>
 </body>
 
 </html>
