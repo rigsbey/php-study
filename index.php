feaa39020c4ed1300e5e28872b7cab701e71d146
Kamil <ab.kam@yandex.ru>
2018-02-08 09:02:17 +0400

    Commit 2 homework

--- index.php
+++ index.php
@@ -1,27 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Задание №2 - числа Фибоначчи </title>
+</head>
+
+<body>
+<form action="index.php" method="get">
+    <b>Введите число N: </b>
+    <input type="text" name="press">
+    <input  type="submit" value="send">
+</form>
+
 <?php
-$name = "Kamil";
-$years = 19;
-$email = "ab.kam@yandex.ru";
-$information = "I want to study some programming language, and English language. It that all (:";
-
-echo "<br><b>задание 1.1</b> <br><br>";
-echo "<b>My name is: </b> $name <br><b>How old i'm:</b> $years<br> <b>My e - mail adress: </b> $email<br> <b> Information about me: </b> $information<br><b>How to contact me?</b><a href='https://vk.com/feed'> my page in vk</a> <br>";
-
-
-
-
-
-$randValue = rand(1,7);
-echo "<br><br><b>задание 1.2</b> <br><br>";
-echo "<b>Выведем, дни от случайного до  понедельника </b><br>";
-echo "<b>Начальное день(случайный): </b><br>";
-switch ($randValue){
-    case "1" : echo "Понедельник <br>"."-Вторник"."-Среда"."-Четверг"."-Пятница"."-Суббота"."-Воскресенье";    break;
-    case "2" : echo "Вторник<br>"."-Среда"."-Четверг"."-Пятница"."-Суббота"."-Воскресенье";break;
-    case "3" : echo "Среда<br>"."-Четверг"."-Пятница"."-Суббота"."-Воскресенье";break;
-    case "4" : echo "Четверг<br>"."-Пятница"."-Суббота"."-Воскресенье";break;
-    case "5" : echo "Пятница<br>"."-Суббота"."-Воскресенье";break;
-    case "6" : echo "Суббота<br>"."-Воскресенье";break;
-    case "7" : echo "Воскресенье<br>"."Это последний день недели! Следующий - Понедельник";break;
+$a = ($_GET['press']);
+$n = 0;
+ function Fibonachi ($n){
+    if($n == 1) return 1;
+    if ($n == 2) return 1;
+    if($n >= 3) return  Fibonachi($n -1) + Fibonachi($n-2);
 }
+
+for($i = 0; $i <= $a; $i++){
+    echo Fibonachi($i);
+    echo " ";
+}
+
 ?>
+</body>
+
+</html>
